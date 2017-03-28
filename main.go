3e13c3e97fe6d32db75c97ce44981c511a8f7510
object88 <object88@gmail.com>
2017-03-27 22:42:22 -0600

    User server and serveMux

--- main.go
+++ main.go
@@ -1,60 +1,72 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 const httpPort = "3000"
 const httpsPort = "3001"
 const graphqlPort = "8081"
 
-var fourohfour string
+var custom404 string
+var highPriorityManifest []Source
+var serveMux *http.ServeMux
 
 func main() {
-	_, highPriorityManifest := loadManifest()
-
-	index, err := loadTemplates(highPriorityManifest)
+	var err error
+	custom404, err = load404()
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println(index)
 
-	fourohfour, err = load404()
-	if err != nil {
-		panic(err.Error())
+	watcher := setupFilewatch()
+	defer watcher.Close()
+
+	serveMux = http.NewServeMux()
+	secureServer := &http.Server{
+		Addr:      ":" + httpsPort,
+		Handler:   serveMux,
+		TLSConfig: &tls.Config{},
 	}
-	fmt.Printf(fourohfour)
 
-	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
-	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
-		fmt.Printf("Serving '%s'\n", req.URL.RequestURI())
-		if req.URL.RequestURI() != "/" {
-			// These are not the droids you're looking for.
-			handle404(resp, req)
-			return
-		}
-		p, ok := resp.(http.Pusher)
-		if ok {
-			for _, v := range highPriorityManifest {
-				p.Push(v.Source, nil)
-			}
+	insecureServer := &http.Server{
+		Addr:    ":" + httpPort,
+		Handler: http.HandlerFunc(redirectToHTTPS),
+	}
+	setupRoutes()
 
-			for _, v := range highPriorityManifest {
-				if v.CSS != nil {
-					p.Push(*v.CSS, nil)
-				}
-			}
-		}
-		fmt.Fprint(resp, index)
-	})
-	go http.ListenAndServeTLS(":"+httpsPort, "cert.pem", "key.pem", nil)
-	http.ListenAndServe(":"+httpPort, http.HandlerFunc(redirectToHTTPS))
+	go secureServer.ListenAndServeTLS("cert.pem", "key.pem")
+	insecureServer.ListenAndServe()
+	done := make(chan struct{})
+	<-done
+	log.Printf("Time to go")
 }
 
 func handle404(resp http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(resp, fourohfour)
+	fmt.Fprintf(resp, custom404)
+}
+
+func push(resp http.ResponseWriter) {
+	p, ok := resp.(http.Pusher)
+	if !ok {
+		return
+	}
+
+	for _, v := range highPriorityManifest {
+		p.Push(v.Source, nil)
+	}
+
+	for _, v := range highPriorityManifest {
+		if v.CSS != nil {
+			p.Push(*v.CSS, nil)
+		}
+	}
 }
 
 func redirectToHTTPS(w http.ResponseWriter, r *http.Request) {
@@ -67,4 +79,65 @@ func redirectToHTTPS(w http.ResponseWriter, r *http.Request) {
 	}
 	dest := fmt.Sprintf("https://%s:%s%s", host, httpsPort, r.RequestURI)
 	http.Redirect(w, r, dest, http.StatusMovedPermanently)
+}
+
+func setupFilewatch() *fsnotify.Watcher {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+					setupRoutes()
+				}
+			case watchErr := <-watcher.Errors:
+				log.Println("watch error:", watchErr)
+			}
+		}
+	}()
+
+	err = watcher.Add("./resources/manifest.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return watcher
+}
+
+func setupRoutes() {
+	_, highPriorityManifest = loadManifest()
+
+	index, err := loadTemplates(highPriorityManifest)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	serveMux.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+
+	serveMux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		if req.URL.RequestURI() != "/" {
+			// These are not the droids you're looking for.
+			handle404(resp, req)
+			return
+		}
+		push(resp)
+		fmt.Fprint(resp, index)
+	})
+
+	// http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+	// http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+	// 	if req.URL.RequestURI() != "/" {
+	// 		// These are not the droids you're looking for.
+	// 		handle404(resp, req)
+	// 		return
+	// 	}
+	// 	push(resp)
+	// 	fmt.Fprint(resp, index)
+	// })
 }
